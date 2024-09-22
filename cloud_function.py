8fda1f8d15e134f449688fa12e7ae6e65afad54c
Ray Lillywhite <raylillywhite@users.noreply.github.com>
2024-09-22 16:43:48 +0800

    WIP: split up server

--- cloud_function.py
+++ cloud_function.py
@@ -2,18 +2,37 @@ import logging
 import functions_framework
 import os
 import argparse
+import requests
+import subprocess
+import threading
+import time
+import contextlib
 
-from engine_utils import handle_job, Engine, ok
+
+from utils import ok
+
+@functions_framework.http
+def handle_job_request(request):
+    job = request.get_json()
+    if not job:
+        logging.error("Invalid job data received")
+        return "Invalid job data", 400
+
+    try:
+        args = get_args()
+        engine = Engine(args)
+        handle_job(args, engine, job)
+        return "Job processed successfully", 200
+    except Exception as e:
+        logging.error("Failed to process job: %s", e)
+        return "Failed to process job", 500
+
 
 def get_args():
     parser = argparse.ArgumentParser(description='Engine Arguments')
-    parser.add_argument('--name', default=os.environ.get('ENGINE_NAME', 'Alpha 2'))
     parser.add_argument('--engine', default=os.environ.get('ENGINE_COMMAND'))
     parser.add_argument('--setoption', nargs='*', default=[])
-    parser.add_argument('--lichess', default=os.environ.get('LICHESS_URL', 'https://lichess.org'))
-    parser.add_argument('--broker', default=os.environ.get('BROKER_URL', 'https://engine.lichess.ovh'))
     parser.add_argument('--token', default=os.environ.get('LICHESS_API_TOKEN'))
-    parser.add_argument('--provider_secret', default=os.environ.get('PROVIDER_SECRET'))
     parser.add_argument('--max_threads', type=int, default=int(os.environ.get('MAX_THREADS', os.cpu_count())))
     parser.add_argument('--max_hash', type=int, default=int(os.environ.get('MAX_HASH', '512')))
     parser.add_argument('--keep_alive', type=int, default=int(os.environ.get('KEEP_ALIVE', '300')))
@@ -31,18 +50,170 @@ def get_args():
 
     return args
 
-@functions_framework.http
-def handle_job_request(request):
-    job = request.get_json()
-    if not job:
-        logging.error("Invalid job data received")
-        return "Invalid job data", 400
-
+def handle_job(args, engine, job):
     try:
-        args = get_args()
-        engine = Engine(args)
-        handle_job(args, engine, job)
-        return "Job processed successfully", 200
-    except Exception as e:
-        logging.error("Failed to process job: %s", e)
-        return "Failed to process job", 500
+        logging.info("Handling job %s", job["id"])
+        with engine.analyse(job) as analysis_stream:
+            ok(requests.post(
+                f"{args.broker}/api/external-engine/work/{job['id']}",
+                data=analysis_stream
+            ))
+    except requests.exceptions.ConnectionError:
+        logging.info("Connection closed while streaming analysis")
+    except requests.exceptions.RequestException as err:
+        logging.error("Error while submitting work: %s", err)
+    except EOFError:
+        logging.error("Engine died")
+
+class Engine:
+    def __init__(self, args):
+        self.process = subprocess.Popen(
+            args.engine,
+            shell=True,
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            bufsize=1,
+            universal_newlines=True
+        )
+        self.args = args
+        self.session_id = None
+        self.hash = None
+        self.threads = None
+        self.multi_pv = None
+        self.uci_variant = None
+        self.supported_variants = []
+        self.last_used = time.monotonic()
+        self.alive = True
+        self.stop_lock = threading.Lock()
+
+        self.uci()
+        self.setoption("UCI_AnalyseMode", "true")
+        self.setoption("UCI_Chess960", "true")
+        for name, value in args.setoption:
+            self.setoption(name, value)
+
+    def idle_time(self):
+        return time.monotonic() - self.last_used
+
+    def terminate(self):
+        self.process.terminate()
+        self.alive = False
+
+    def send(self, command):
+        logging.debug("%d << %s", self.process.pid, command)
+        self.process.stdin.write(command + "\n")
+        self.process.stdin.flush()
+
+    def recv(self):
+        while True:
+            line = self.process.stdout.readline()
+            if line == "":
+                self.alive = False
+                raise EOFError()
+
+            line = line.rstrip()
+            if not line:
+                continue
+
+            logging.debug("%d >> %s", self.process.pid, line)
+
+            command_and_params = line.split(None, 1)
+
+            if len(command_and_params) == 1:
+                return command_and_params[0], ""
+            else:
+                return command_and_params
+
+    def uci(self):
+        self.send("uci")
+        while True:
+            command, args = self.recv()
+            if command == "option":
+                name = None
+                args = args.split()
+                while args:
+                    arg = args.pop(0)
+                    if arg == "name":
+                        name = args.pop(0)
+                    elif name == "UCI_Variant" and arg == "var":
+                        self.supported_variants.append(args.pop(0))
+            elif command == "uciok":
+                break
+
+        if self.supported_variants:
+            logging.info("Supported variants: %s", ", ".join(self.supported_variants))
+
+    def isready(self):
+        self.send("isready")
+        while True:
+            line, _ = self.recv()
+            if line == "readyok":
+                break
+
+    def setoption(self, name, value):
+        self.send(f"setoption name {name} value {value}")
+
+    @contextlib.contextmanager
+    def analyse(self, job):
+        work = job["work"]
+
+        if work["sessionId"] != self.session_id:
+            self.session_id = work["sessionId"]
+            self.send("ucinewgame")
+            self.isready()
+
+        options_changed = False
+        if self.threads != work["threads"]:
+            self.setoption("Threads", work["threads"])
+            self.threads = work["threads"]
+            options_changed = True
+        if self.hash != work["hash"]:
+            self.setoption("Hash", work["hash"])
+            self.hash = work["hash"]
+            options_changed = True
+        if self.multi_pv != work["multiPv"]:
+            self.setoption("MultiPV", work["multiPv"])
+            self.multi_pv = work["multiPv"]
+            options_changed = True
+        if self.uci_variant != work["variant"]:
+            self.setoption("UCI_Variant", work["variant"])
+            self.uci_variant = work["variant"]
+            options_changed = True
+        if options_changed:
+            self.isready()
+
+        self.send(f"position fen {work['initialFen']} moves {' '.join(work['moves'])}")
+
+        go_options = ""
+        for key in ["movetime", "depth", "nodes"]:
+            if key in work:
+                go_options += f" {key} {work[key]}"
+                break  # Only one time control is used
+
+        self.send(f"go{go_options}")
+
+        def stream():
+            while True:
+                command, params = self.recv()
+                if command == "bestmove":
+                    break
+                elif command == "info":
+                    if "score" in params:
+                        yield (command + " " + params + "\n").encode("utf-8")
+                else:
+                    logging.warning("Unexpected engine command: %s", command)
+
+        analysis = stream()
+        try:
+            yield analysis
+        finally:
+            self.stop()
+            for _ in analysis:
+                pass
+
+        self.last_used = time.monotonic()
+
+    def stop(self):
+        if self.alive:
+            with self.stop_lock:
+                self.send("stop")
--- main.py
+++ main.py
@@ -3,13 +3,9 @@ import requests
 import time
 import threading
 import os
+import secrets
 import argparse
-
-from engine_utils import (
-    setup_http_session,
-    register_engine,
-    invoke_cloud_function,
-)
+from utils import ok, setup_http_session
 
 def get_args():
     parser = argparse.ArgumentParser(description='Engine Arguments')
@@ -19,7 +15,6 @@ def get_args():
     parser.add_argument('--lichess', default=os.environ.get('LICHESS_URL', 'https://lichess.org'))
     parser.add_argument('--broker', default=os.environ.get('BROKER_URL', 'https://engine.lichess.ovh'))
     parser.add_argument('--token', default=os.environ.get('LICHESS_API_TOKEN'))
-    parser.add_argument('--provider_secret', default=os.environ.get('PROVIDER_SECRET'))
     parser.add_argument('--max_threads', type=int, default=int(os.environ.get('MAX_THREADS', os.cpu_count())))
     parser.add_argument('--max_hash', type=int, default=int(os.environ.get('MAX_HASH', '512')))
     parser.add_argument('--keep_alive', type=int, default=int(os.environ.get('KEEP_ALIVE', '300')))
@@ -39,6 +34,46 @@ def get_args():
         exit(1)
 
     return args
+
+
+def register_engine(args, http):
+    res = ok(http.get(f"{args.lichess}/api/external-engine"))
+
+    secret = secrets.token_urlsafe(32)
+
+    variants = {
+        "chess",
+        "antichess",
+        "atomic",
+        "crazyhouse",
+        "horde",
+        "kingofthehill",
+        "racingkings",
+        "3check",
+    }
+
+    # Engine instance is needed to get supported variants
+    supported_variants = ["chess"]
+
+    registration = {
+        "name": args.name,
+        "maxThreads": args.max_threads,
+        "maxHash": args.max_hash,
+        "variants": [variant for variant in supported_variants if variant in variants],
+        "providerSecret": secret,
+    }
+
+    for engine_data in res.json():
+        if engine_data["name"] == args.name:
+            logging.info("Updating engine %s", engine_data["id"])
+            ok(http.put(f"{args.lichess}/api/external-engine/{engine_data['id']}", json=registration))
+            break
+    else:
+        logging.info("Registering new engine")
+        ok(http.post(f"{args.lichess}/api/external-engine", json=registration))
+
+    return secret
+
 
 def poll_for_work(args, http, secret):
     logging.info("Starting the work polling loop")
@@ -61,6 +96,15 @@ def poll_for_work(args, http, secret):
         except Exception as e:
             logging.error("Error while polling for work: %s", e)
             time.sleep(args.poll_interval)  # Wait before retrying in case of an error
+
+            
+def invoke_cloud_function(cloud_function_url, job):
+    try:
+        res = requests.post(cloud_function_url, json=job)
+        res.raise_for_status()
+        logging.info("Cloud function invoked successfully")
+    except requests.exceptions.RequestException as err:
+        logging.error("Error invoking cloud function: %s", err)
 
 def main():
     args = get_args()
